31b0ae99bed8193ecc553a939fd560aebaac5a28
ShadePhantom <fo19031903@gmail.com>
2024-12-14 01:49:00 +0300

    character

--- StartGate/Assets/Scripts/Player/PlayerMovement.cs
+++ StartGate/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,9 +7,10 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 movement;
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 3;
-
+    private Vector3 originalScale;
     private void Start()
     {
+        originalScale = transform.localScale;
         rb =this.gameObject.GetComponent<Rigidbody2D>();
 
     }
@@ -25,6 +26,16 @@ public class PlayerMovement : MonoBehaviour
 
         rb.MovePosition(rb.position + normalizedMovement * moveSpeed * Time.fixedDeltaTime);
 
+        if (movement.x > 0)
+        {
+            // Saða gidiyorsa
+            transform.localScale = new Vector3(Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
+        else if (movement.x < 0)
+        {
+            // Sola gidiyorsa
+            transform.localScale = new Vector3(-Mathf.Abs(originalScale.x), originalScale.y, originalScale.z);
+        }
     }
 
 }
