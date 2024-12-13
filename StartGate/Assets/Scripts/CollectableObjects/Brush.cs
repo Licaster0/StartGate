8a9125c67b9c109fd378dd8bae4d8539e360f46d
ShadePhantom <fo19031903@gmail.com>
2024-12-13 23:13:53 +0300

    İnteract

--- StartGate/Assets/Scripts/CollectableObjects/Brush.cs
+++ StartGate/Assets/Scripts/CollectableObjects/Brush.cs
@@ -1,10 +1,14 @@
 
+using TMPro;
 using UnityEngine;
 
 public class Brush : MonoBehaviour , ICollectable
 {
+    public string brush;
+    public TMP_Text TMP_Text;
     public void Collect()
     {
-       
+       Debug.Log("fasklþdfjþsalkf");
     }
+
 }
