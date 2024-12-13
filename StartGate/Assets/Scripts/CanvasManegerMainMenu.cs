afd49b9bebe5dceee0d3f904efe5e819ed2a0f69
Licaster0 <ahmet0434recep@gmail.com>
2024-12-13 20:55:09 +0300

    Main Menu

--- StartGate/Assets/Scripts/CanvasManegerMainMenu.cs
+++ StartGate/Assets/Scripts/CanvasManegerMainMenu.cs
@@ -1,20 +1,67 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.TextCore.Text;
 
 public class Canvas : MonoBehaviour
 {
+    [SerializeField] private GameObject creditAnimation;
+    [SerializeField] private CanvasGroup creditPanel;
+    [SerializeField] private float fadeDuration = 0.5f; 
+    [SerializeField] private bool isPanelVisible = false;
+
+    private void Start()
+    {
+
+    }
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
     }
 
-    public void CreditGame()
+    public void ToggleCreditPanel()
     {
+        if (isPanelVisible)
+        {
+            StartCoroutine(FadeOut());
+        }
+        else
+        {
+            StartCoroutine(FadeIn());
+        }
 
+        isPanelVisible = !isPanelVisible;
     }
+
+    private IEnumerator FadeIn()
+    {
+        creditPanel.gameObject.SetActive(true);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            creditPanel.alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        creditPanel.alpha = 1f;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            creditPanel.alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        creditPanel.alpha = 0f;
+        creditPanel.gameObject.SetActive(false); 
+    }
+
 
     public void QuitGame()
     {
