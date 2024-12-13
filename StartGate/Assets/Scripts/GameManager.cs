bb9d4279884241e3fca557be5d2c136acfd97a4d
Özgür <ozgur.calli52@gmail.com>
2024-12-13 23:40:22 +0300

    memorybook

--- StartGate/Assets/Scripts/GameManager.cs
+++ StartGate/Assets/Scripts/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    public PlayerMovement player;
     private void Awake()
     {
         if (Instance == null)
@@ -12,17 +14,17 @@ public class GameManager : MonoBehaviour
             Instance = this;
         }
         else
-        { 
-            Destroy(gameObject);    
+        {
+            Destroy(gameObject);
         }
     }
     void Start()
     {
-        
+
     }
-  
+
     void Update()
     {
-        
+
     }
 }
