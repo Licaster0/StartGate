5cfd70cea9d0a63dda2c2dabe71c6c6e32caf910
Licaster0 <ahmet0434recep@gmail.com>
2024-12-13 20:11:48 +0300

    main screen

--- StartGate/Assets/Scripts/CanvasManegerMainMenu.cs
+++ StartGate/Assets/Scripts/CanvasManegerMainMenu.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
 
 public class Canvas : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void StartGame()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+    
+    private void CreditGame()
+    {
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
