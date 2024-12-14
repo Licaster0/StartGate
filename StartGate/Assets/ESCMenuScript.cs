ef13dc13ee0021f8513b9634605ad623ca601d46
Licaster0 <ahmet0434recep@gmail.com>
2024-12-14 11:00:12 +0300

    esc

---
+++ StartGate/Assets/ESCMenuScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ESCMenuScript : MonoBehaviour
+{
+    public GameObject pauseMenuPanel; // Menü panelini buraya atayacaðýz.
+    private bool isPaused = false; // Menü durumu
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Update()
+    {
+        // ESC tuþuna basýldýðýnda
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+    
+    void PauseGame()
+    {
+        pauseMenuPanel.SetActive(true); // Menü panelini aç
+        Time.timeScale = 0f; // Oyunu duraklat
+        isPaused = true;
+    }
+
+    void ResumeGame()
+    {
+        pauseMenuPanel.SetActive(false); // Menü panelini kapat
+        Time.timeScale = 1f; // Oyunu devam ettir
+        isPaused = false;
+    }
+
+}
