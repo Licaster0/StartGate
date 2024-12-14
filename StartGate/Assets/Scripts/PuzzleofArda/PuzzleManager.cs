548f7ecd1ab1e0db8b2a3bfa1648313273666a72
Arda <macelik3434@gmail.com>
2024-12-14 12:02:29 +0300

    putzle

--- StartGate/Assets/Scripts/PuzzleofArda/PuzzleManager.cs
+++ StartGate/Assets/Scripts/PuzzleofArda/PuzzleManager.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour
 {
-    public SnapToPosition[] puzzlePieces;
+
+    public GameObject p1;
+    public GameObject p2;
+    public GameObject p3;
+    public GameObject p4;
+    public GameObject p5;
+    public Transform p1pos;
+    public Transform p2pos;
+    public Transform p3pos;
+    public Transform p4pos;
+    public Transform p5pos;
 
     private void Update()
     {
-        if (IsPuzzleComplete())
+        if (p1.transform.position == p1pos.position && p2.transform.position == p2pos.position && p3.transform.position == p3pos.position && p4.transform.position == p4pos.position && p5.transform.position == p5pos.position)
         {
-            Debug.Log("Puzzle Tamamlandı!");
-            // Oyun tamamlandıktan sonra yapılacak işlemler
+            Debug.Log("Puzzle tamamlandı");
         }
     }
 
-    private bool IsPuzzleComplete()
-    {
-        foreach (SnapToPosition piece in puzzlePieces)
-        {
-            if (!piece.enabled) return false;
-        }
-        return true;
-    }
+
+
+
+
 }
