5abddfc919b967cc2fe14875d983f0a081f78135
Arda <macelik3434@gmail.com>
2024-12-14 14:11:50 +0300

    afafsafa

--- StartGate/Assets/onmousescene.cs
+++ StartGate/Assets/onmousescene.cs
@@ -1,23 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class onmousescene : MonoBehaviour
 {
-    public string scenename;
     private void OnMouseDown()
     {
         SceneManager.LoadScene("puzzle2");
-    }
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
