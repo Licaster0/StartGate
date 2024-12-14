6c5a90d90f9aa104accabe6e8e575183370d25ec
Özgür <ozgur.calli52@gmail.com>
2024-12-14 10:28:06 +0300

    .

---
+++ StartGate/Assets/Scripts/Anı2/Tas.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tas : MonoBehaviour
+{
+    private bool isTriggered = false;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "LampLight")
+        {
+            isTriggered = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (isTriggered)
+        {
+
+        }
+    }
+}
