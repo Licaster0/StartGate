9d5a18bd88147691f3eb1298329f8e5e314edcea
Arda <macelik3434@gmail.com>
2024-12-14 11:25:39 +0300

    sdasada

---
+++ StartGate/Assets/Scripts/PuzzleofArda/PuzzleManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public SnapToPosition[] puzzlePieces;
+
+    private void Update()
+    {
+        if (IsPuzzleComplete())
+        {
+            Debug.Log("Puzzle Tamamlandı!");
+            // Oyun tamamlandıktan sonra yapılacak işlemler
+        }
+    }
+
+    private bool IsPuzzleComplete()
+    {
+        foreach (SnapToPosition piece in puzzlePieces)
+        {
+            if (!piece.enabled) return false;
+        }
+        return true;
+    }
+}
---
+++ StartGate/Assets/Scripts/PuzzleofArda/SnapToPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToPosition : MonoBehaviour
+{
+    public Transform targetPosition; // Doğru yerleşim noktası
+    public float snapDistance = 0.5f; // Hedefe yakınlık eşiği
+
+    private bool isPlaced = false;
+
+
+    private void Update()
+    {
+        if (!isPlaced && Vector3.Distance(transform.position, targetPosition.position) < snapDistance)
+        {
+            transform.position = targetPosition.position; // Parçayı hedefe sabitle
+        }
+    }
+   
+}
