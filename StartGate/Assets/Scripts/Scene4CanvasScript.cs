0dbb54bfade0528b5bd0a4aa57bad993e43b427e
Licaster0 <ahmet0434recep@gmail.com>
2024-12-14 12:11:55 +0300

    312

--- StartGate/Assets/Scripts/Scene4CanvasScript.cs
+++ StartGate/Assets/Scripts/Scene4CanvasScript.cs
@@ -1,17 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Scene4CanvasScript : MonoBehaviour
 {
-    [SerializeField] GameObject book;
-    [SerializeField] Transform target;
-    public void MemoryBook()
+    [SerializeField] private GameObject bookPrefab;
+    [SerializeField] private Transform target;
+    [SerializeField] private Sprite switchOnSprite;
+    [SerializeField] private Sprite switchOffSprite;
+
+    private GameObject spawnedBook;
+    private Image spriteImage;
+
+    private void Start()
     {
-        if (book != null && target != null)
+        // Bu objedeki SpriteRenderer bileþenini al
+        spriteImage = GameObject.Find("Test").GetComponent<Image>();
+
+        // Baþlangýçta switch kapalý görünümüyle baþla
+        if (spriteImage != null && switchOffSprite != null)
         {
-            Instantiate(book, target.position, target.rotation);
+            spriteImage.sprite = switchOffSprite;
         }
     }
 
+    public void MemoryBook()
+    {
+        if (spawnedBook == null)
+        {
+            // Kitap objesi oluþturuluyor
+            spawnedBook = Instantiate(bookPrefab, target.position, target.rotation);
+
+            // Sprite'i "açýk" hale getir
+            if (spriteImage != null && switchOnSprite != null)
+            {
+                spriteImage.sprite = switchOnSprite;
+            }
+        }
+        else
+        {
+            // Kitap objesi siliniyor
+            Destroy(spawnedBook);
+            spawnedBook = null;
+
+            // Sprite'i "kapalý" hale getir
+            if (spriteImage != null && switchOffSprite != null)
+            {
+                spriteImage.sprite = switchOffSprite;
+            }
+        }
+    }
 }
