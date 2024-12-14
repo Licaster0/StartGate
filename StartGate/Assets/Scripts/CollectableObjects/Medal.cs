c5b652753d9d7a602b508ecdba13ed3f4806cb4f
Özgür <ozgur.calli52@gmail.com>
2024-12-14 23:40:40 +0300

    yollabakem

--- StartGate/Assets/Scripts/CollectableObjects/Medal.cs
+++ StartGate/Assets/Scripts/CollectableObjects/Medal.cs
@@ -14,6 +14,6 @@ public class Medal : MonoBehaviour, ICollectable
     private IEnumerator enumerator()
     {
         yield return new WaitForSeconds(2.5f);
-        SceneManager.LoadScene(3);
+        SceneManager.LoadScene(4);
     }
 }
