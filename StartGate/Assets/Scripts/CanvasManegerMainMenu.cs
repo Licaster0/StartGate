f005f14f5a4fcae961ace8b2eacee0cf3314cccb
Licaster0 <ahmet0434recep@gmail.com>
2024-12-14 15:21:47 +0300

    main screen

--- StartGate/Assets/Scripts/CanvasManegerMainMenu.cs
+++ StartGate/Assets/Scripts/CanvasManegerMainMenu.cs
@@ -8,7 +8,8 @@ public class Canvas : MonoBehaviour
     [SerializeField] private CanvasGroup creditPanel;
     [SerializeField] private float fadeDuration = 0.5f; 
     [SerializeField] private bool isPanelVisible = false;
-
+    [SerializeField] private bool isSettingsPanelVisible = false;
+    [SerializeField] private CanvasGroup settingsPanel;
     private void Start()
     {
 
@@ -32,6 +33,48 @@ public class Canvas : MonoBehaviour
         isPanelVisible = !isPanelVisible;
     }
 
+    public void ToggleSettingsPanel()
+    {
+        if (isSettingsPanelVisible)
+        {
+            StartCoroutine(SettingsFadeOut());
+        }
+        else
+        {
+            StartCoroutine(SettingsFadeIn());
+        }
+
+        isSettingsPanelVisible = !isSettingsPanelVisible;
+    }
+    private IEnumerator SettingsFadeIn()
+    {
+        settingsPanel.gameObject.SetActive(true);
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            settingsPanel.alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        settingsPanel.alpha = 1f;
+    }
+
+    private IEnumerator SettingsFadeOut()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            settingsPanel.alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeDuration);
+            yield return null;
+        }
+
+        settingsPanel.alpha = 0f;
+        settingsPanel.gameObject.SetActive(false);
+    }
     private IEnumerator FadeIn()
     {
         creditPanel.gameObject.SetActive(true);
