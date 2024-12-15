2d26e79257285078c19722a22657875420dfcc8d
ShadePhantom <fo19031903@gmail.com>
2024-12-15 13:29:01 +0300

    abcdefg

--- StartGate/Assets/Scripts/CollectableObjects/OldPicture.cs
+++ StartGate/Assets/Scripts/CollectableObjects/OldPicture.cs
@@ -7,14 +7,26 @@ using UnityEngine.SceneManagement;
 public class OldPicture : MonoBehaviour, ICollectable
 {
     public TMP_Text m_Text;
+    private bool m_IsPlaying = false;
     public void Collect()
     {
-        m_Text.text = "Sevgilim seni ihmal etmemeliydim...";
-        StartCoroutine(enumerator());
+        if (!m_IsPlaying)
+        {
+            m_Text.text = "Sevgilim seni ihmal etmemeliydim...";
+
+            StartCoroutine(enumerator());
+            m_IsPlaying = true;
+        }
+        else
+        {
+            m_Text.text = "Hatani anlayip duzelttin";
+        }
+       
     }
     private IEnumerator enumerator()
     {
         yield return new WaitForSeconds(2.5f);
+        
         SceneManager.LoadScene(7);
     }
 }
